407b431ae46617819fbc5c43df4e75bff2915a94
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-03-03 14:35:58 +0530

    File updated at 3/3/2025, 2:35:56 PM

--- 136-SingleNumber/136-SingleNumber.cpp
+++ 136-SingleNumber/136-SingleNumber.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        if(n==1) return nums[0];
-        for(int i=0;i<n;i+=2){
-            if(nums[i]!=nums[i+1]){
-                return nums[i];
-            }
-        }
-        return 0;
+       int res=0;
+       for(int i:nums){
+        res^=i;
+       }
+       return res;
     }
 };
