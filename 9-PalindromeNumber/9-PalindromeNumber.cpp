6722bd63642ef79c71f7dec7f0fb5d8d88c066a2
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-03-06 19:34:34 +0530

    File updated at 3/6/2025, 7:34:34 PM

--- 9-PalindromeNumber/9-PalindromeNumber.cpp
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -7,8 +7,9 @@ public:
         int tem=x;
         while(x>0){
             int r=x%10;
-            if (rev > (INT_MAX-r)/10) return false;
+             if(rev>(INT_MAX-r)/10) return false;
             rev= rev*10 + r;
+           
             x/=10;
         }
         return rev==tem;
