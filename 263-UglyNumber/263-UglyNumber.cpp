9262617a9b9d905825e0a6868d56c0aea0d2d003
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-03-13 12:03:42 +0530

    File updated at 3/13/2025, 12:03:41 PM

--- 263-UglyNumber/263-UglyNumber.cpp
+++ 263-UglyNumber/263-UglyNumber.cpp
@@ -1,35 +1,18 @@
 class Solution {
-    void primeFactorArr(vector<int>&arr,int n){
-         if(n%2==0){
-        arr.push_back(2);
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+
         while(n%2==0){
             n/=2;
         }
-       }
-       for(long long int i=3;(long long) i*i<=n;i+=2){
-        if(n%i==0){
-            arr.push_back(i);
-            while(n%i==0){
-                n/=i;
-            }
+        while(n%3==0){
+            n/=3;
         }
-       }
-       if(n>1) arr.push_back(n);
-    }
-public:
-    bool isUgly(int n) {
-        vector<int>arr;
-        if(n<=0) return false;
-        if(n>0 && n<=2) return true;
-      primeFactorArr(arr,n);
-
-
-      for(auto it:arr){
-        cout<<it<<" ";
-      }
-      for(int i=0;i<arr.size();i++){
-        if(arr[i]>5) return false;
-      }
-      return true;
+        while(n%5==0){
+            n/=5;
+        }
+        return n==1;
     }
 };
