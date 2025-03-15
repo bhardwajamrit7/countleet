8fbd616f16db9ec2e8866f1130d2c4aedd665f27
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-03-15 21:27:56 +0530

    Create file

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long long int quotient= (long long int) dividend/divisor;
+        if(quotient>INT_MAX) return INT_MAX;
+        if(quotient<INT_MIN) return INT_MIN;
+        return quotient;
+    }
+};
