c5389e82d26cc6f16485c915ef32282a773e132f
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-04-13 17:09:50 +0530

    File updated at 4/13/2025, 5:09:49 PM

--- 10-RegularExpressionMatching/10-RegularExpressionMatching.cpp
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.cpp
@@ -1,30 +1,39 @@
-// Last updated: 4/12/2025, 9:24:06 PM
+// Last updated: 4/13/2025, 5:09:49 PM
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int m = s.size(), n = p.size();
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
-        dp[0][0] = true;
-
-        for (int j = 2; j <= n; j++) {
-            if (p[j - 1] == '*') {
-                dp[0][j] = dp[0][j - 2];
-            }
+        if(p.length() == 0){
+            return p.length() == s.length();
         }
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i][j - 2];
-                    if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
-                        dp[i][j] = dp[i][j] || dp[i - 1][j];
-                    }
+        if(p.length() > 1 && p[1] == '*'){
+            char cur = p[0];
+
+            int index = 0;
+            while(index < s.length() && (cur == '.' || cur == s[index])){
+                index++;
+            }
+
+            for(int i = 0; i <= index; i++){
+                if(isMatch(s.substr(i), p.substr(2))){
+                    return true;
                 }
             }
+
+            return false;
+
         }
 
-        return dp[m][n];
+        if(s.length() == 0) return false;
+
+        if(p[0] == '.'){
+            return isMatch(s.substr(1), p.substr(1));
+        }
+
+        if(p[0] == s[0]){
+            return isMatch(s.substr(1), p.substr(1));
+        }
+
+        return false;
     }
 };
