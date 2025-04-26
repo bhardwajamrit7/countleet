f117865413c4a30127ee338944fb274b70088be6
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-04-26 23:16:30 +0530

    Create file

---
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 4/26/2025, 11:16:29 PM
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        for (int num : nums) {
+            minHeap.push(num);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+
+        return minHeap.top();        
+    }
+};
