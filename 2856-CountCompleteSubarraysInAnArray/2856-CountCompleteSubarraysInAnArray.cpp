d687ae169f01bde1ae7db4a489db1d34aea951ec
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-06-16 11:46:20 +0530

    Create 2856-CountCompleteSubarraysInAnArray

---
+++ 2856-CountCompleteSubarraysInAnArray/2856-CountCompleteSubarraysInAnArray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/16/2025, 11:46:14 AM
+class Solution {
+    public:
+        int countCompleteSubarrays(vector<int>& nums) {
+            int k = unordered_set<int>(nums.begin(),nums.end()).size(), res = 0;
+            for (int i = 0; i < nums.size(); i++) {
+                unordered_set<int> st;
+                for (int j = i; j < nums.size(); j++) {
+                    st.insert(nums[j]);
+                    if (st.size() == k) res++;
+                }
+            }
+            return res;
+        }
+    };
