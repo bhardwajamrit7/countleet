a4ef42d58bb7abe0aa5b9f9a365414f17d8193df
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-06-23 09:40:30 +0530

    File updated at 6/23/2025, 9:40:28 AM

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,18 +1,16 @@
-// Last updated: 6/16/2025, 11:49:15 AM
+// Last updated: 6/23/2025, 9:40:28 AM
 class Solution {
 public:
-     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-    map<int,int>mp;
-    for(int i=0;i<n;i++){
-        if(mp[target-nums[i]]>0){
+    vector<int> twoSum(vector<int>& nums, int target) {
         vector<int>ans;
-        ans.push_back(mp[target-nums[i]]-1);
-       ans.push_back(i);
-        return ans;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
         }
-        mp[nums[i]]=i+1;
+        return ans;
     }
-   return {-1};
-     }
 };
