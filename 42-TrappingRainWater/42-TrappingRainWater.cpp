4dd3da7a110f6bec6c9971f884abd87fd3e1e69c
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-06-26 10:12:48 +0530

    File updated at 6/26/2025, 10:12:47 AM

--- 42-TrappingRainWater/42-TrappingRainWater.cpp
+++ 42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -1,25 +1,24 @@
-// Last updated: 6/16/2025, 11:48:24 AM
+// Last updated: 6/26/2025, 10:12:47 AM
 class Solution {
 public:
    int trap(vector<int>& arr) {
         int n=arr.size();
-        int pr[n];
-        // vector<int>pr(n);
-        pr[0]=arr[0];
-        for(int i=1;i<n;i++){
-            pr[i]=max(pr[i-1],arr[i]);
-        }
-        int sf[n];
-        // vector<int>sf(n);
-        sf[n-1]=arr[n-1];
-        for(int i=n-2;i>=0;i--){
-            sf[i]=max(sf[i+1],arr[i]);
-        }
+       vector<int>pr(n);
+       pr[0]=arr[0];
+       for(int i=1;i<n;i++){
+        pr[i]=max(pr[i-1],arr[i]);
+       }
 
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=min(pr[i],sf[i])-arr[i];
-        }
+       vector<int>suf(n);
+
+       suf[n-1]=arr[n-1];
+       for(int i=n-2;i>=0;i--){
+        suf[i]=max(suf[i+1],arr[i]);
+       }
+       int sum=0;
+       for(int i=0;i<n;i++){
+        sum+=min(pr[i],suf[i])-arr[i];
+       }
         return sum;
     }
 };
