78a1f423cfbf17e0adb0104e6e65bc04748b733b
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-07-01 10:11:14 +0530

    File updated at 7/1/2025, 10:11:13 AM

--- 852-PeakIndexinaMountainArray/852-PeakIndexinaMountainArray.cpp
+++ 852-PeakIndexinaMountainArray/852-PeakIndexinaMountainArray.cpp
@@ -1,24 +1,14 @@
-// Last updated: 7/1/2025, 10:08:02 AM
+// Last updated: 7/1/2025, 10:11:13 AM
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int n=arr.size()-1;
-        int low=0;
-        int high=n-1;
-        // int peak=-1;
+        int n=arr.size();
+        int i=0;
 
-        while(low<=high){
-           int mid=low+(high-low)/2;
-            if(arr[mid]<arr[mid+1]){
-                low=mid+1;
-            }
-           else if(arr[mid]<arr[mid-1]){
-                high=mid-1;
-            }
-            else if(arr[mid]>arr[mid+1]){
-                return mid;
-            }
+        while(i<n-1 && arr[i]<arr[i+1]){
+            i++;
+         
         }
-        return -1;
+        return i;
     }
 };
