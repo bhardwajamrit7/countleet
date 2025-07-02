376a25da479d62ad98178c7e7448b1b6fbab68a9
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-07-02 10:19:37 +0530

    File updated at 7/2/2025, 10:19:36 AM

--- 162-FindPeakElement/162-FindPeakElement.cpp
+++ 162-FindPeakElement/162-FindPeakElement.cpp
@@ -1,15 +1,20 @@
+// Last updated: 7/2/2025, 10:19:36 AM
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n=nums.size();
-        if(n==1) return 0;
-        if(nums[0]>nums[1]) return 0;
-        if(nums[n-1]>nums[n-2]) return n-1;
-        for(int i=1;i<n-1;i++){
-            if((nums[i]>nums[i-1])&&(nums[i]>nums[i+1])){
-                return i;
+        int low=0;
+        int high=n-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<nums[mid+1] ){
+                low=mid+1;      
             }
+            else{
+                high=mid;
+            }
+
         }
-        return -1;
+        return low;
     }
 };
