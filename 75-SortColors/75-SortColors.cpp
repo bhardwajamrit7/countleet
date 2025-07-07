79139e1d903aeda5f81e70e6bb4b16fa5bc5c918
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-07-07 22:51:05 +0530

    File updated at 7/7/2025, 10:51:05 PM

--- 75-SortColors/75-SortColors.cpp
+++ 75-SortColors/75-SortColors.cpp
@@ -1,21 +1,22 @@
-// Last updated: 6/16/2025, 11:48:04 AM
+// Last updated: 7/7/2025, 10:51:05 PM
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low = 0, mid = 0, high = nums.size()-1;
-        while(mid <= high){
-            if(nums[mid] == 0){
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
-            }
-            else if(nums[mid] == 1){
-                mid++;
-            }
-            else{
-                swap(nums[mid], nums[high]);
-                high--;
-            }
+        int n=nums.size();
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        for(auto a:nums){
+            if(a==0) count0++;
+            else if(a==1) count1++;
+            else count2++;
         }
+
+        for(int i=0;i<n;i++){
+            if(i<count0) nums[i]=0;
+            else if(i<count0+count1) nums[i]=1;
+            else nums[i]=2;
+        }
+
     }
 };
