413b29709ad2e6be0c6a676cdbe8205a36ed213c
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-08-18 16:51:50 +0530

    File updated at 8/18/2025, 4:51:49 PM

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,17 +1,15 @@
-// Last updated: 8/18/2025, 4:38:04 PM
+// Last updated: 8/18/2025, 4:51:49 PM
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-        int start=0;
+        int end=0;
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                nums[start]=nums[i];
-                start++;
+               swap(nums[i],nums[end]);
+               end++;
             }
         }
-        for(int i=start;i<n;i++){
-            nums[i]=0;
-        }
+       
     }
 };
