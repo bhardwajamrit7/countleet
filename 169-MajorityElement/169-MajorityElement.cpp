04f8d5a8c6c95f87ff9c255c77e3035982ac4c88
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-08-19 12:28:09 +0530

    File updated at 8/19/2025, 12:28:07 PM

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,27 +1,9 @@
-// Last updated: 6/16/2025, 11:47:37 AM
+// Last updated: 8/19/2025, 12:28:07 PM
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int c=0,ans=0;
-        for(int i=0;i<nums.size();i++){
-            if(c==0){
-                c=1;
-                ans=nums[i];
-            }
-            else if(nums[i]==ans)
-            {
-                c++;
-            }
-            else{
-                c--;
-            }
-        }
-
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==ans) count++;
-        }
-        if(count>(nums.size())/2) return ans;
-        else return -1;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        return nums[n/2];
     }
 };
