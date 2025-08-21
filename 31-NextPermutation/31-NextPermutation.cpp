5c80a7fa8d1c73bf589855dd25bea0d9c49ae29f
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-08-21 13:49:53 +0530

    File updated at 8/21/2025, 1:49:52 PM

--- 31-NextPermutation/31-NextPermutation.cpp
+++ 31-NextPermutation/31-NextPermutation.cpp
@@ -1,7 +1,33 @@
-// Last updated: 8/21/2025, 1:10:14 PM
+// Last updated: 8/21/2025, 1:49:52 PM
 class Solution {
+    void reverse(vector<int>&arr,int low,int high){
+        while(low<=high){
+            swap(arr[low++],arr[high--]);
+        }
+
+    }
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+       int n=nums.size();
+       int ind=-1;
+       for(int i=n-2;i>=0;--i){
+        if(nums[i]<nums[i+1]){
+            ind=i;
+            break;
+        }
+       }
+        if(ind==-1){
+        reverse(nums,0,n-1);
+        return;
+       }
+       for(int i=n-1;i>ind;--i){
+        if(nums[i]>nums[ind]){
+            swap(nums[i],nums[ind]);
+            break;
+        }
+       }
+      
+       reverse(nums,ind+1,n-1);
+
     }
 };
