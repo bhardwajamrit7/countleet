bfc9a7397a7b61661f9de138900fe711d2ea4d03
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-08-22 22:36:21 +0530

    File updated at 8/22/2025, 10:36:19 PM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
@@ -1,39 +1,29 @@
-// Last updated: 8/22/2025, 10:19:07 PM
+// Last updated: 8/22/2025, 10:36:19 PM
 class Solution {
-    void visRow(int i,vector<vector<int>>&mat,int m){
-        for(int j=0;j<m;j++){
-            if(mat[i][j]!=0){
-                mat[i][j]=-777788;
-            }
-        }
-    }
-    void visCol(int j,vector<vector<int>>&mat,int n){
-        for(int i=0;i<n;i++){
-            if(mat[i][j]!=0){
-                mat[i][j]=-777788;
-            }
-        }
-    }
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         if(m==0) return ;
         int n=matrix[0].size();
         if (n == 0) return;  
+
+        vector<int>Row(m,0);
+        vector<int>Col(n,0);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    visRow(i,matrix,n);
-                    visCol(j,matrix,m);
+                    Row[i]=1;
+                    Col[j]=1;
                 }
             }
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==-777788){
+                if(Row[i]==1 || Col[j]==1){
                     matrix[i][j]=0;
                 }
             }
         }
+
     }
 };
