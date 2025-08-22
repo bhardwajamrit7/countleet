bc50cc8f6ee722bab9c41890dca5d0850ca991f9
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-08-22 23:03:15 +0530

    File updated at 8/22/2025, 11:03:13 PM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/22/2025, 10:36:19 PM
+// Last updated: 8/22/2025, 11:03:13 PM
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -6,22 +6,40 @@ public:
         if(m==0) return ;
         int n=matrix[0].size();
         if (n == 0) return;  
-
-        vector<int>Row(m,0);
-        vector<int>Col(n,0);
+        int col_0=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    Row[i]=1;
-                    Col[j]=1;
+
+                  matrix[i][0]=0;
+                  if(j!=0){
+                    matrix[0][j]=0;
+                  }
+                  else{
+                    col_0=0;
+                  }
+                }
+
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(Row[i]==1 || Col[j]==1){
-                    matrix[i][j]=0;
-                }
+
+        if(matrix[0][0]==0){
+        for(int j=0;j<n;j++){
+            matrix[0][j]=0;
+        }
+        }
+        if(col_0==0){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
             }
         }
 
