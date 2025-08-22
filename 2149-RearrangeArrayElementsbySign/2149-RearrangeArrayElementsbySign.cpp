9c291078c4e204eaa7aa1aca47c658a0b07a739e
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-08-23 00:01:08 +0530

    File updated at 8/23/2025, 12:01:06 AM

--- 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.cpp
+++ 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.cpp
@@ -1,20 +1,24 @@
-// Last updated: 6/24/2025, 9:42:55 AM
+// Last updated: 8/23/2025, 12:01:06 AM
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n=nums.size();
        vector<int>ans(n);
-        int even=0,odd=1;
+        int j=0;
        for(int i=0;i<n;i++){
         if(nums[i]>=0){
-            ans[even]=nums[i];
-            even+=2;
-        }
-        else{
-            ans[odd]=nums[i];
-            odd+=2;
+            ans[j]=nums[i];
+            j+=2;
         }
        }
+       int k=1;
+        for(int l=0;l<n;l++){
+            if(nums[l]<0){
+                ans[k]=nums[l];
+                 k+=2;
+            }
+           
+        }
         return ans;
     }
 };
