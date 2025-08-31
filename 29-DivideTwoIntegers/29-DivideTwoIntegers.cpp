0ab1ec17c99be197734a4ebb77e28ded5c914458
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-08-31 21:31:33 +0530

    File updated at 8/31/2025, 9:31:33 PM

--- 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
@@ -1,35 +1,33 @@
-// Last updated: 8/31/2025, 9:06:18 PM
+// Last updated: 8/31/2025, 9:31:33 PM
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (dividend == INT_MIN && divisor == -1) 
+        if(dividend==INT_MIN && divisor== -1){
             return INT_MAX;
-
-        bool negative = (dividend < 0) ^ (divisor < 0);
-
-        long long a = dividend;
-        long long b = divisor;
-
-        a = (a < 0) ? -a : a;
-        b = (b < 0) ? -b : b;
-
-        long long result = 0;
-
-        while (a >= b) {
-            long long temp = b, multiple = 1;
-            while (a >= (temp << 1)) {
-                temp <<= 1;
-                multiple <<= 1;
-            }
-            a -= temp;
-            result += multiple;
         }
+        bool neg=(dividend<0)^(divisor<0);
+        long long x = dividend;
+        long long y = divisor;
 
-        if (negative) result = -result;
+        x=(x<0)?-x:x;
+        y=(y<0) ?-y:y;
 
-        if (result > INT_MAX) return INT_MAX;
-        if (result < INT_MIN) return INT_MIN;
+        long long int res=0;
+        while(x>=y){
+            long long temp=y,mul=1;
+            while(x>=(temp<<1)){
+                temp <<=1;
+                mul<<=1; 
+            }
+            x-=temp;
+            res+=mul;
+        }
+        if (neg)res= -res;
+        if(res>INT_MAX) return INT_MAX;
+        if(res<INT_MIN) return INT_MIN;
 
-        return (int)result;
+        return (int)res;
+
+        return neg ? -res : res;
     }
 };
