29ad1842ab1dc5114e55d4f772b85b58e07935be
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-09-13 11:08:44 +0530

    File updated at 9/13/2025, 11:08:44 AM

--- 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.cpp
+++ 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.cpp
@@ -1,4 +1,4 @@
-// Last updated: 9/13/2025, 10:59:26 AM
+// Last updated: 9/13/2025, 11:08:44 AM
 class Solution {
 public:
     int maxFreqSum(string s) {
@@ -6,27 +6,28 @@ public:
         int cons=0;
         string vs="aeiouAEIOU";
         map<char,int>mpV;
-        map<char,int>mpC;
+        // map<char,int>mpC;
         for(auto c:s){
-            if(vs.find(c)!=string::npos){
-                mpV[c]++;
-            }
-            else{
-                mpC[c]++;
-            }
+           mpV[c]++;
+           cout<<mpV[c]<<" ";
         }
-        int f1=0;
+        int f1=0,f2=0;
         for(auto &it:mpV){
-            if(it.second>f1){
-                f1=it.second;
+            if((vs.find(it.first)!=string::npos)){
+                cout<<it.first<<" "<<it.second<<endl;
+                if(it.second>f1)
+                    f1=it.second;
             }
-        }
-        int f2=0;
-        for(auto &it:mpC){
-            if(it.second>f2){
+            else if(it.second>f2){
                 f2=it.second;
             }
         }
+        // int f2=0;
+        // for(auto &it:mpC){
+        //     if(it.second>f2){
+        //         f2=it.second;
+        //     }
+        // }
 
         return f1+f2;
 
