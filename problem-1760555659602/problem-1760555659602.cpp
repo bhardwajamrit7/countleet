9cfb27aa39ccfa06f2df2a0d21642078a5345230
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-10-16 00:44:21 +0530

    Create

---
+++ problem-1760555659602/problem-1760555659602.cpp
@@ -0,0 +1,27 @@
+// Last updated: 10/16/2025, 12:44:19 AM
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pref(n, 1), suff(n, 1);
+
+        // pref[i] = length of increasing subarray ending at i
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1])
+                pref[i] = pref[i - 1] + 1;
+        }
+
+        // suff[i] = length of increasing subarray starting at i
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1])
+                suff[i] = suff[i + 1] + 1;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++) {
+            ans = max(ans, min(pref[i], suff[i + 1]));
+        }
+
+        return ans;
+    }
+};
