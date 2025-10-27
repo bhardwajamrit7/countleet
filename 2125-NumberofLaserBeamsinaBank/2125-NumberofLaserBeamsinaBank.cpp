1a843eea29c90340ff4727b96b57ecd222e2f639
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-10-27 22:42:15 +0530

    File updated at 10/27/2025, 10:42:14 PM

--- 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.cpp
+++ 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.cpp
@@ -1,31 +1,35 @@
-// Last updated: 10/27/2025, 10:39:00 PM
+// Last updated: 10/27/2025, 10:42:14 PM
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
         int n=bank.size();
         vector<int>cnt;
+        int prev=0,sum=0;
        for(auto num:bank){
         int count=0;
             for(int i=0;i<num.size();i++){
                 if(num[i]=='1')count++;
             }
-            cnt.push_back(count);
+            if(count>0){
+                sum+=prev*count;
+                prev=count;
+            }
        }
 
-       int sum=0;
-       for(int i=0;i<cnt.size();i++){
-        if(cnt[i]!=0){
-            for(int j=i+1;j<cnt.size();j++){
-                if(cnt[j]!=0){
-                    sum+=cnt[i]*cnt[j];
-                    break;
-                }
-                else{
-                    continue;
-                }
-            }
-        }
-       }
+    //    int sum=0;
+    //    for(int i=0;i<cnt.size();i++){
+    //     if(cnt[i]!=0){
+    //         for(int j=i+1;j<cnt.size();j++){
+    //             if(cnt[j]!=0){
+    //                 sum+=cnt[i]*cnt[j];
+    //                 break;
+    //             }
+    //             else{
+    //                 continue;
+    //             }
+    //         }
+    //     }
+    //    }
        return sum;
     }
 };
