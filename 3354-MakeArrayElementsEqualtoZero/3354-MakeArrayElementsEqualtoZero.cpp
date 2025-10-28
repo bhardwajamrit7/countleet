dc0eb06da0fe1af9b28e46e13b4940d2443147fd
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-10-28 21:49:52 +0530

    File updated at 10/28/2025, 9:49:51 PM

--- 3354-MakeArrayElementsEqualtoZero/3354-MakeArrayElementsEqualtoZero.cpp
+++ 3354-MakeArrayElementsEqualtoZero/3354-MakeArrayElementsEqualtoZero.cpp
@@ -1,4 +1,4 @@
-// Last updated: 10/28/2025, 9:44:55 PM
+// Last updated: 10/28/2025, 9:49:51 PM
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
@@ -11,20 +11,20 @@ public:
         int curr=0;
         int res=0;
         for(int i=0;i<n;i++){
-            curr+=nums[i];
-            
+            if(nums[i]!=0){
+                curr+=nums[i];
+            }
+            else{
                 int left=curr;
                 int right=sum-left;
-            if(nums[i]!=0){
-                continue;
-            }
 
                 if(right==left){
                     res+=2;
                 }
-                if(abs(right-left)==1){
+                else if(abs(right-left)==1){
                     res+=1;
-                }      
+                }
+            }
         }
         return res;
     }
