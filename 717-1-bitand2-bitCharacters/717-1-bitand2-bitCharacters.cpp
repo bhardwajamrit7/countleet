cded09134354a4dace1046f1d1e25ae987bf9b9c
Amrit Kumar <115589483+bhardwajamrit7@users.noreply.github.com>
2025-11-18 21:11:40 +0530

    Create 717-1-bitand2-bitCharacters

---
+++ 717-1-bitand2-bitCharacters/717-1-bitand2-bitCharacters.cpp
@@ -0,0 +1,12 @@
+// Last updated: 11/18/2025, 9:11:39 PM
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+        int i = 0;        
+        while (i < n - 1)
+            i += bits[i] + 1;
+
+        return i == n - 1;
+    }
+};
